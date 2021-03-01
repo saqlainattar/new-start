96ad187bcd3a8ab8302461266178cd1ec29f2071
saqlainattar <saqlainattar123@gmail.com>
2021-03-01 17:08:24 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,37 @@
+var player1 = prompt('player one: Enter Name, Ble');
+var player1Color = 'rgb(86, 151, 255)';
+
+var player2 = prompt('player Two: Enter Name, Red');
+var player2Color = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
+
+function reportWin(rowNum,colNum) {
+	console.log('u won starting at this row,col');
+	console.log(rowNum);
+	console.log(colNum);
+}
+
+function changeColor(rowIndex,colIndex,color){
+	return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
+}
+
+function returnColor(rowIndex,colIndex){
+	return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+
+function checkBottom(colIndex) {
+ 	var colorReport = returnColor(5,colIndex);
+ 	for (var row = 5; row > -1; row--) {
+ 		colorReport = returnColor(row,colIndex);
+ 		if (colorReport === 'rgb(128, 128, 128)') {
+ 			return row
+ 		}
+ 	}
+ }
+
+ function colorMatchCheck(one,two,three,four) {
+ 	return (one === two && one === three && one === four && one !== 'rgb(128, 128, 128)'  && one !== undefined);
+ 	 
+ }
